5bb152a7b8c479b54435a5559df9d4cf3cc3a8e1
DanielASJr <89815489+DanielASJr@users.noreply.github.com>
2021-11-24 00:15:32 -0300

    Delete Exercício_26.py

--- Exercício_26.py
+++
@@ -1,9 +0,0 @@
-num = int(input('Digite um número para a tabuada: '))
-while(num<=0):
-    print("Erro! Somente números de 1 a 10")
-    num = int(input('Digite um número para a  tabuada: '))
-i = 1
-while(i<11):
-    r = num * i
-    print(f'{num} X {i} = {r}')
-    i = i + 1 
